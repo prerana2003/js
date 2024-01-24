6264c62fcb9d9851a53f6535db5f70cd572ccf9c
Prerana Divekar <prerana.div2003@gmail.com>
2024-01-24 18:37:30 +0000

    commit

--- Zero_at_StartOrEnd.js
+++ Zero_at_StartOrEnd.js
@@ -1,11 +1,12 @@
-let arr = [1,0,4,3,0,2,0,1,3,8,0,4,6,9,4,1,2,5];
+let arr = [1,0,4,3,0,2,0,1,3,8,0,4,6,9,4,1,2,5,8];
 let k = "start";
 
 const moving_zeros = (arr,k) =>{
-    let p = "start";
+    let p = "end";
     let zeroCount= 0;
     let resArr = []
     if(p === k){
+    // ------------------zeros at start--------------------
         for(let i=0;i<arr.length;i++){
             if(arr[i] !== 0){
                 for(let j=i+1;j<arr.length;j++){
@@ -19,22 +20,17 @@ const moving_zeros = (arr,k) =>{
                 }
             }
         }
-        console.log(zeroCount)
-        console.log(arr.length)
         
         resArr = arr.slice(0,zeroCount)
-        // arr.splice(0,zeroCount)
+        arr.splice(0,zeroCount)
 
-        for(let i=zeroCount;i<arr.length;i++){
-            for(let j=i+1;j<arr.length;j++){
-                if(arr[i] === arr[j]){
-                    // arr.splice
-                }
+        for(let i=0;i<arr.length;i++){
+            if(!resArr.includes(arr[i])){
+                resArr.push(arr[i])
             }
         }
-
-        for(let i=zeroCount;i<arr.length;i++){
-            for(let j=i+1;j<arr.length;j++){
+        for(let i=0;i<resArr.length;i++){
+            for(let j=i+1;j<resArr.length;j++){
                 if(arr[i]>arr[j]){
                     temp = arr[i];
                     arr[i] = arr[j];
@@ -43,6 +39,8 @@ const moving_zeros = (arr,k) =>{
             }
         }
     }
+
+// ------------------zeros at end--------------------
     else{
         for(let i=arr.length-1;i>=0;i--){
             if(arr[i] !== 0){
@@ -51,14 +49,32 @@ const moving_zeros = (arr,k) =>{
                         temp = arr[i];
                         arr[i] = arr[j];
                         arr[j] = temp;
+                        zeroCount++;
                         break;
                     }
                 }
             }
         }
-    }
 
-    return [resArr,arr];
+        resArr = arr.slice(-zeroCount)
+
+        for(let i=0;i<arr.length;i++){
+            if(!resArr.includes(arr[i])){
+                resArr.push(arr[i])
+            }
+        }
+
+        for(let i=0;i<resArr.length;i++){
+            for(let j=i+1;j<resArr.length;j++){
+                if(resArr[i]<resArr[j]){
+                    temp = resArr[i];
+                    resArr[i] = resArr[j];
+                    resArr[j] = temp
+                }
+            }
+        }
+    }
+    return resArr;
 }
 
 console.log(moving_zeros(arr,k))
